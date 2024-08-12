be7bdb9eddbcb12c8016f543e7361bb093aeb673
nomemolestes <knh3755@naver.com>
2024-08-13 00:16:11 +0900

    20240813 백준 18108 태국식 나이계산

---
+++ bakjoon_/src/bakjoon_/p_no_18108.java
@@ -0,0 +1,11 @@
+package bakjoon_;
+
+import java.util.Scanner;
+
+public class p_no_18108 {
+	 public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int curAge = sc.nextInt(); //한국나이
+		System.out.println(curAge-543);
+	}
+}
