355ba61817aaf4c6db392305bd0e9d27e7ef3b25
nomemolestes <knh3755@naver.com>
2024-08-14 22:53:43 +0900

    20240814 백준 조건문 비교연산자

---
+++ bakjoon_/src/bakjoon_/p_no_9498.java
@@ -0,0 +1,21 @@
+package bakjoon_;
+
+import java.util.Scanner;
+
+public class p_no_9498 {
+	 public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A = sc.nextInt();
+		if(90 <= A && A <= 100){
+			System.out.println("A");
+		} else if(80 <= A && A <= 89){
+			System.out.println("B");
+		} else if(70 <= A && A <= 79){
+			System.out.println("C");
+		} else if(60 <= A && A <= 69){
+			System.out.println("D");
+		} else {
+			System.out.println("F");
+		}
+	}
+}
