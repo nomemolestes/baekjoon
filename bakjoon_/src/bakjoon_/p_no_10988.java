51a55b12d7d61cf1bba7499ffce8966123af191b
nomemolestes <knh3755@naver.com>
2025-02-14 11:17:38 +0900

    20250214 심화 10988번 작성중 커밋테스트

--- bakjoon_/src/bakjoon_/p_no_10988.java
+++ bakjoon_/src/bakjoon_/p_no_10988.java
@@ -9,11 +9,11 @@ public class p_no_10988 {
         int sum = 0;
         for(int i=0; i < arr.length; i++) {
         	if(i < arr.length) { //i = 0~9
-        		if() {
+//        		if() {
 		        	if(arr[i].equals(arr[i+1])) {
 		        		sum ++;
 		        	}
-        		}
+//        		}
         	}
         }
         System.out.println(sum);
