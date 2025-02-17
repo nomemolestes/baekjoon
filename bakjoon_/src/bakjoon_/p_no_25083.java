56b8d6a92a2ed430cd37bfc2a70706158b15bb27
nomemolestes <knh3755@naver.com>
2025-02-17 11:04:23 +0900

    20250217 심화1 새싹출력하기

---
+++ bakjoon_/src/bakjoon_/p_no_25083.java
@@ -0,0 +1,13 @@
+package bakjoon_;
+
+public class p_no_25083 {
+	 public static void main(String[] args) {
+		//새싹출력
+		System.out.println("         ,r'\"7");
+		System.out.println("r`-_   ,'  ,/");
+		System.out.println(" \\. \". L_r'");
+		System.out.println("   `~\\/");
+		System.out.println("      |");
+		System.out.println("      |");
+	}
+}
